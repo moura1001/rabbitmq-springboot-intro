533a2c760d36bc4fc447d3d732b86af13097442f
moura1001 <gmouraneto01@gmail.com>
2022-03-12 17:03:45 -0300

    commit com configuração inicial do microsserviço do consumer

---
+++ consumer/src/test/java/com/github/moura1001/microservice/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.moura1001.microservice.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
