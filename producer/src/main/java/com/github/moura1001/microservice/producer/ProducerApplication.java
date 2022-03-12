2b656fbe73ba561935db4fc4ceec7c403d9853b3
moura1001 <gmouraneto01@gmail.com>
2022-03-12 12:49:07 -0300

    commit com finalização do producer

--- producer/src/main/java/com/github/moura1001/microservice/producer/ProducerApplication.java
+++ producer/src/main/java/com/github/moura1001/microservice/producer/ProducerApplication.java
@@ -1,13 +1,24 @@
 package com.github.moura1001.microservice.producer;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class ProducerApplication implements CommandLineRunner {
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		rabbitTemplate.convertAndSend("RabbitExchange", "RabbitRouting", "Rabbit message");		
 	}
 
 }
