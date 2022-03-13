cdf2eb1b64f3263ac9f7138d4e13980b8f0d4f42
moura1001 <gmouraneto01@gmail.com>
2022-03-13 11:29:15 -0300

    commit com configuração de exchange do tipo topic e binding com a devida queue

--- consumer/src/main/java/com/github/moura1001/microservice/consumer/config/RabbitConfig.java
+++ consumer/src/main/java/com/github/moura1001/microservice/consumer/config/RabbitConfig.java
@@ -1,5 +1,9 @@
 package com.github.moura1001.microservice.consumer.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -16,6 +20,8 @@ import com.github.moura1001.microservice.consumer.service.ConsumerService;
 public class RabbitConfig {
 	
 	private static final String QUEUE = "RabbitQueue";
+	private static final String EXCHANGE = "RabbitExchange";
+	private static final String ROUTING_KEY = "RabbitRouting";
 	
 	@Value("${spring.rabbitmq.host}")
 	private String host;
@@ -27,6 +33,23 @@ public class RabbitConfig {
 	@Bean
 	public Queue rabbitQueue() {
 		return new Queue(QUEUE, true);
+	}
+	
+	@Bean
+	public Exchange rabbitExchange() {
+		return ExchangeBuilder
+				.topicExchange(EXCHANGE)
+				.durable(true)
+				.build();
+	}
+	
+	@Bean
+	public Binding binding() {
+		return BindingBuilder
+				.bind(rabbitQueue())
+				.to(rabbitExchange())
+				.with(ROUTING_KEY)
+				.noargs();
 	}
 	
 	@Bean
